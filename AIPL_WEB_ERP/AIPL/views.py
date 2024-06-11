6101431f2597c34fecc2e11920c31787781c5744
CypherNinja1010 <priyesh.kumarjii@gmail.com>
2024-06-11 09:08:53 +0530

    Everything is working fine || next step is to add migrations

--- AIPL_WEB_ERP/AIPL/views.py
+++ AIPL_WEB_ERP/AIPL/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home_page_view(request):
+    response = "Home Page Initial Setup"
+    return render(
+        request,
+        "home_page.html",
+        {"response":response}
+    )
