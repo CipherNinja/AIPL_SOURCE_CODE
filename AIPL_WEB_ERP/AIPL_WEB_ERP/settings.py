d808cb342aff2d714cb96f0e3f2aa5db1dd6bcd2
CypherNinja1010 <priyesh.kumarjii@gmail.com>
2024-06-22 18:05:22 +0530

    Configured urls.py and settings.py with C Panel

--- AIPL_WEB_ERP/AIPL_WEB_ERP/settings.py
+++ AIPL_WEB_ERP/AIPL_WEB_ERP/settings.py
@@ -125,9 +125,8 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
-    BASE_DIR / 'AIPL' / 'FrontEnd' / 'static',  
+    BASE_DIR / 'AIPL' / 'FrontEnd' / 'static',
 ]
 
 MEDIA_URL = '/media/'
