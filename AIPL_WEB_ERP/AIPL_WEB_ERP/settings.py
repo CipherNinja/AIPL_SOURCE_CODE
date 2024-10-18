ec4a2640e0136a66bca8aac96f5aa656e63a47ef
CypherNinja1010 <priyesh.kumarjii@gmail.com>
2024-10-18 10:58:00 +0530

    db update

--- AIPL_WEB_ERP/AIPL_WEB_ERP/settings.py
+++ AIPL_WEB_ERP/AIPL_WEB_ERP/settings.py
@@ -101,8 +101,8 @@ DATABASES = {
 	'default': {
 		'ENGINE': 'django.db.backends.mysql',
 		'NAME': 'agratasi_agratasinfotech', # agratasi_agratasinfotech
-		'USER': 'agratasi_agratasinfotech', # agratasi_agratasinfotech
-		'PASSWORD': 'agratas9069076975', # agratas9069076975
+		'USER': 'agratasi_root', # agratasi_root
+		'PASSWORD': '9069076975', # agratas9069076975
 		'HOST':'localhost',
 		'PORT':'3306',
 	}
