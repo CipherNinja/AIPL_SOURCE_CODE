f565fc84816938707dbb90847f9ec3a6b8f2732a
CypherNinja1010 <priyesh.kumarjii@gmail.com>
2024-11-03 23:48:04 +0530

    Admin Panel

--- AIPL_WEB_ERP/AIPL_WEB_ERP/settings.py
+++ AIPL_WEB_ERP/AIPL_WEB_ERP/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["agratasinfotech.com","www.agratasinfotech.com"]
+ALLOWED_HOSTS = ['127.0.0.1',"agratasinfotech.com","www.agratasinfotech.com"]
 
 # Application definition
 
