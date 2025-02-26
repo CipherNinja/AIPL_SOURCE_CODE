53ff86efa503368b8742d77386b4bc1eb6d1a1ed
Priyesh Pandey <91175201+CipherNinja@users.noreply.github.com>
2025-02-26 23:36:22 +0530

    Update settings.py

--- AIPL_WEB_ERP/AIPL_WEB_ERP/settings.py
+++ AIPL_WEB_ERP/AIPL_WEB_ERP/settings.py
@@ -85,7 +85,7 @@ MIDDLEWARE.insert(0, 'django.middleware.security.SecurityMiddleware')
 SECURE_CONTENT_TYPE_NOSNIFF = True  # Prevents browsers from MIME-sniffing files
 SECURE_BROWSER_XSS_FILTER = True    # Enables XSS filtering by the browser
 
-Enforce HTTPS
+# Enforce HTTPS
 SECURE_SSL_REDIRECT = True
 SECURE_HSTS_SECONDS = 31536000  # 1 year; adjust based on preference
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
